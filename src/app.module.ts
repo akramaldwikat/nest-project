fba295f0425ca94240ac98bef8be0e75868a2e1a
akramaldwikat <akram.aldwikat1@gmail.com>
2025-03-25 14:11:44 +0300

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateUserModule } from './create-user/create-user.module';
 
 @Module({
-  imports: [],
+  imports: [CreateUserModule],
   controllers: [AppController],
   providers: [AppService],
 })
